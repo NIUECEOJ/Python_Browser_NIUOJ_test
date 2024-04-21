3efdccefe3af786df5afc67d08f907fb923fd66a
Goldbeibei <167362633+NIUECEOJ@users.noreply.github.com>
2024-04-21 10:00:51 +0800

    更新 Web_Browser.py

--- Python_Browser_NIUOJ_test/Web_Browser.py
+++ Python_Browser_NIUOJ_test/Web_Browser.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtWidgets import QApplication, QMainWindow, QSplitter
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 class MainWindow(QMainWindow):
     def __init__(self):
