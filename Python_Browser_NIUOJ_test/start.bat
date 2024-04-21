7b6e233a769a39391a3cfea97d0731b556615263
Goldbeibei <167362633+NIUECEOJ@users.noreply.github.com>
2024-04-21 09:11:02 +0800

    更新 start.bat

--- Python_Browser_NIUOJ_test/start.bat
+++ Python_Browser_NIUOJ_test/start.bat
@@ -4,7 +4,7 @@ chcp 65001 > nul
 for /f "delims=" %%i in ('where python') do set "python_exe=%%i"
 
 if not defined python_exe (
-    echo 無法找到 Python 解釋器。
+    echo Python interpreter not found.
     pause
     exit /b 1
 )
@@ -12,33 +12,32 @@ if not defined python_exe (
 set "python_path=%python_exe%"
 set "script_path=.\Web_Browser.py"
 
-echo 正在安裝必要的 Python 庫...
+echo Installing required Python libraries...
+%python_path% -m pip install --upgrade pip
 %python_path% -m pip install PyQt5
 %python_path% -m pip install PyQtWebEngine
 
 :restart
-echo 正在以系統管理員身份運行 Python 腳本...
-
+echo Running Python script with admin privileges...
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 
 if '%errorlevel%' == '0' (
     goto run_script
 ) else (
-    echo 請求管理員權限...
+    echo Requesting admin privileges...
     goto get_admin
 )
 
 :get_admin
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    exit /b
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /b
 
 :run_script
 echo %python_path%
 echo %script_path%
-python "%~dp0%script_path%"
+"%python_path%" "%~dp0%script_path%"
 
 :check_script
 timeout /t 0.1 > nul
