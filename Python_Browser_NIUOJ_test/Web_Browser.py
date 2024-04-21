ccb40ff2bc85325c909d7d87c6ca9925a75ddf75
Goldbeibei <167362633+NIUECEOJ@users.noreply.github.com>
2024-04-21 11:14:42 +0800

    加入檢查上線時間

--- Python_Browser_NIUOJ_test/Web_Browser.py
+++ Python_Browser_NIUOJ_test/Web_Browser.py
@@ -1,6 +1,8 @@
 import sys
-from PyQt5.QtCore import Qt, QUrl
-from PyQt5.QtWidgets import QApplication, QMainWindow, QSplitter
+import os
+import time
+from PyQt5.QtCore import Qt, QUrl, QTimer
+from PyQt5.QtWidgets import QApplication, QMainWindow, QSplitter, QInputDialog
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 class MainWindow(QMainWindow):
@@ -19,6 +21,28 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(splitter)
         self.showFullScreen()
+
+        # Check status.log
+        if os.path.exists('status.log'):
+            with open('status.log', 'r') as f:
+                last_status = f.readlines()[-1]
+                last_time = float(last_status.split(',')[0])
+                if time.time() - last_time < 10:
+                    password, ok = QInputDialog.getText(self, 'Password Input', 'Enter your password:')
+                    if not ok or password != 'Jefery':
+                        sys.exit()
+        else:
+            with open('status.log', 'w') as f:
+                f.write(f'{time.time()},online\n')
+
+        # Start status logging
+        self.start_logging()
+        print(os.path.abspath('status.log'))
+
+    def start_logging(self):
+        with open('status.log', 'a') as f:
+            f.write(f'{time.time()},online\n')
+        QTimer.singleShot(1000, self.start_logging)
 
 app = QApplication(sys.argv)
 window = MainWindow()
